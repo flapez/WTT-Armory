64e710f90f5cf477b6c470219e965366a0ed1ea5
GrooveypenguinX <jakechase2110@gmail.comX>
2025-12-27 19:16:59 -0500

    u saw nothing

--- WTT-Armory/Helpers/ArmoryQuestHelper.cs
+++ WTT-Armory/Helpers/ArmoryQuestHelper.cs
@@ -127,7 +127,7 @@ namespace WTTArmory.Helpers
 
             // Connections Up North (6764174c86addd02bc033d68)
             AddWeaponsToKillCondition(quests, "6764174c86addd02bc033d68", [
-                REMINGTON_MSR, CHEYTAC_M200, XM109, M1894, M700_XL_ACTION, M700_XL_ACTION_NORMA, AXMC_300_WINMAG, HENRY_BIG_BOY
+                REMINGTON_MSR, CHEYTAC_M200,  M1894, M700_XL_ACTION, M700_XL_ACTION_NORMA, AXMC_300_WINMAG, HENRY_BIG_BOY
             ]);
 
             // ====================== PEACEKEEPER QUESTS ======================
@@ -146,7 +146,7 @@ namespace WTTArmory.Helpers
 
             var tarkovShooterWeapons = new[]
             {
-                REMINGTON_MSR, CHEYTAC_M200, XM109, M1894, M700_XL_ACTION, M700_XL_ACTION_NORMA, AXMC_300_WINMAG, HENRY_BIG_BOY
+                REMINGTON_MSR, CHEYTAC_M200,  M1894, M700_XL_ACTION, M700_XL_ACTION_NORMA, AXMC_300_WINMAG, HENRY_BIG_BOY
             };
 
             // Tarkov Shooter Part 1-8
@@ -168,12 +168,12 @@ namespace WTTArmory.Helpers
 
             // Psycho Sniper (5c0be13186f7746f016734aa)
             AddWeaponsToKillCondition(quests, "5c0be13186f7746f016734aa", [
-                REMINGTON_MSR, CHEYTAC_M200, XM109, M1894, M700_XL_ACTION, M700_XL_ACTION_NORMA, AXMC_300_WINMAG, HENRY_BIG_BOY
+                REMINGTON_MSR, CHEYTAC_M200,  M1894, M700_XL_ACTION, M700_XL_ACTION_NORMA, AXMC_300_WINMAG, HENRY_BIG_BOY
             ]);
 
             // Shooter Born in Heaven (5c0bde0986f77479cf22c2f8)
             AddWeaponsToKillCondition(quests, "5c0bde0986f77479cf22c2f8", [
-                REMINGTON_MSR, CHEYTAC_M200, XM109, M1894, M700_XL_ACTION, M700_XL_ACTION_NORMA, AXMC_300_WINMAG, HENRY_BIG_BOY
+                REMINGTON_MSR, CHEYTAC_M200,  M1894, M700_XL_ACTION, M700_XL_ACTION_NORMA, AXMC_300_WINMAG, HENRY_BIG_BOY
             ]);
 
             // Make Amends Equipment (6261482fa4eb80027c4f2e11)
